86413e09881f3c186ce2a3a512b23b58277ee8db
Marcy Student <Marcy_Student@MARCY-MBAir40.local>
2025-11-24 15:53:02 -0500

    Section 2 completed.

--- src/bank.js
+++ src/bank.js
@@ -9,20 +9,19 @@ class BankAccount {
 
   deposit(amount) {
     this.#balance += amount; // Balance is privete so we need to use #.
-    console.log(`Deposited $${amount}. New balance: $${this.#balance}`); //Balance is privete so we need to use #.
-    return; // We only need to declare return.
+    console.log(`Deposited $${amount}. New balance: $${this.#balance}`); // Balance is privete so we need to use #.
+    // We only need to declare return.
   }
 
   withdraw(amount) {
     if (amount > this.#balance) { // Balance is privete so we need to use #.
       console.log(`Withdrawal failed. Insufficient funds.`);
-      return; // We need this return so if it didn't meet this requirment we return it right away.
+      // We need this return so if it didn't meet this requirment we return it right away.
     } else {
       this.#balance -= amount; // Balance is privete so we need to use #.
       console.log(`Withdrew $${amount}. New balance: $${this.#balance}`);// Balance is privete so we need to use #.
-      return this.#balance; // We need this inside of the withdraw method. 
+      return this.#balance; // We need this inside of the withdraw method.
     } // Balance is privete so we need to use #.
-   
   }
 
   getBalance() {
@@ -39,40 +38,37 @@ class Bank {
 
   constructor(name) {
     this.name = name;
-    Bank.accounts.push(this)
-  }
-  
-  addAccount(account) {
-    // We need to add the name of the class that we are trying to add to it. 
-     Bank.accounts.push(account); 
+    Bank.accounts.push(this);
   }
 
-    static getTotalBalance() { // This need to be static for safty reason. 
+  addAccount(account) {
+    // We need to add the name of the class that we are trying to add to it.
+    Bank.accounts.push(account);
+  }
+
+  static getTotalBalance() { // This need to be static for safty reason.
     let total = 0;
     this.accounts.forEach((account) => {
       total += accounts.balance;
-      
     });
-     return total;
-   
+    return total;
   }
 
-  static findAccount(accountNumber) { // This need to be static for safty reasons. 
+  static findAccount(accountNumber) { // This need to be static for safty reasons.
     return Bank.accounts.find((account) => account.accountNumber === accountNumber); // We need strict equals for comparison
-  } // We have to add Bank class because it's inside of the Bank class. 
+  } // We have to add Bank class because it's inside of the Bank class.
 }
 
 // TEST YOUR CODE HERE
 
-
 // DO NOT REMOVE
 module.exports = { BankAccount, Bank };
 
-const myBank = new Bank("First National");
+const myBank = new Bank('First National');
 console.log(myBank); // Bank { name: "First National" }
 
-const account1 = new BankAccount("001", "Alice");
-const account2 = new BankAccount("002", "Bob");
+const account1 = new BankAccount('001', 'Alice');
+const account2 = new BankAccount('002', 'Bob');
 console.log(account1); // BankAccount { accountNumber: "001", ownerName: "Alice" }
 console.log(account2); // BankAccount { accountNumber: "002", ownerName: "Bob" }
 
@@ -90,4 +86,4 @@ console.log(account2.deposit(250)); // Deposited $250. New Balance: 250
 console.log(myBank.getTotalBalance()); // 300
 
 // Find a specific account by accountNumber
-console.log(myBank.findAccount("001").ownerName); // "Alice"
+console.log(myBank.findAccount('001').ownerName); // "Alice"
--- src/pokemon.js
+++ src/pokemon.js
@@ -1,52 +1,53 @@
 class Pokemon {
-    static allPokemon = [];
+  static allPokemon = [];
 
-    #health = 100;
-    #level = 1;
+  #health = 100;
 
-constructor(name, type){
-    this.name = name
-    this.type = type
-    Pokemon.allPokemon.push(this)
+  #level = 1;
+
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+    Pokemon.allPokemon.push(this);
+  }
+
+  getHealth() {
+    return this.#health;
+  }
+
+  getLevel() {
+    return this.#level;
+  }
+
+  levelUp() {
+    this.#level += 1;
+    this.#health += 10;
+    console.log(`${this.name} leveled up to level ${this.#level}!`);
+  }
+
+  isFainted() {
+    if (this.#health <= 0) {
+      return true;
     }
-    getHealth(){
-        return this.#health;
-    }
-    getLevel(){
-        return this.#level;
-    }
-    levelUp(){
-         this.#level += 1
-        this.#health += 10
-       console.log(`${this.name} leveled up to level ${this.#level}!`)
-    }
-    isFainted(){
-        if (this.#health <= 0){
-            return true
-        } else {
-            false
-        }
-    }
-    attack(targetPokemon){
-        this.#health -= 10
-        console.log(`${this.name} attacked ${target.name}!`)
-    }
-    static getTotalPokemon(){
-        return Pokemon.allPokemon.length;
-    }
-    static findByName(name){
-        return Pokemon.allPokemon.find((p) => p.name === name)
-    } 
+    false;
+  }
+
+  static getTotalPokemon() {
+    return Pokemon.allPokemon.length;
+  }
+
+  static findByName(name) {
+    return Pokemon.allPokemon.find((p) => p.name === name);
+  }
 }
-
 
 // TEST YOUR CODE HERE
 
 // DO NOT REMOVE
 module.exports = { Pokemon };
 
-const charizard = new Pokemon("Charizard", "Fire");
-const squirtle = new Pokemon("Squirtle", "Water");
+const charizard = new Pokemon('Charizard', 'Fire');
+const squirtle = new Pokemon('Squirtle', 'Water');
 // 1. Checking instance properties
 console.log(charizard); // Pokemon { name: "Charizard", type: "Fire" }
 console.log(squirtle); // Pokemon { name: "Squirtle", type: "Water" }
@@ -59,10 +60,10 @@ console.log(squirtle.getLevel()); // 2
 // OR if you use the get syntax
 console.log(squirtle.level); // 2
 // 4. Checking on health
-console.log(squirtle.getHealth());   // Should be 100
+console.log(squirtle.getHealth()); // Should be 100
 // OR if you use the get syntax
-console.log(squirtle.health);   // Should be 100
+console.log(squirtle.health); // Should be 100
 // 6. Finding a Pokemon instance
-console.log(Pokemon.findByName("Charizard")); // Pokemon { name: "Charizard", type: "Fire" }
-//7. Viewing count of all Pokemon
-console.log("Total Pokemon:", Pokemon.getTotalPokemon()); // 2
+console.log(Pokemon.findByName('Charizard')); // Pokemon { name: "Charizard", type: "Fire" }
+// 7. Viewing count of all Pokemon
+console.log('Total Pokemon:', Pokemon.getTotalPokemon()); // 2
